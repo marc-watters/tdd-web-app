f5eebf78185f0a1e8fd81c7a44ddfaf6c64842ab
Marc Watters <marc.watters@proton.me>
2025-02-07 15:39:07 -0500

    rfxr(feat): utilize helper GetPlayerScore()

--- server.go
+++ server.go
@@ -8,14 +8,9 @@ import (
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprint(w, GetPlayerScore(player))
+}
 
-	if player == "Pepper" {
-		fmt.Fprint(w, "20")
-		return
-	}
-	if player == "Floyd" {
-		fmt.Fprint(w, "10")
-		return
 func GetPlayerScore(player string) (score string) {
 	switch player {
 	case "Pepper":
