be4679bb119845919ef0967d8464c8792c0c8e14
Marc Watters <marc.watters@proton.me>
2025-02-07 15:42:13 -0500

    fix(cmd): implement InMemoryPlayerStore.GetPlayerScore()

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 
 type InMemoryPlayerStore struct{}
 
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
 	server := &PlayerServer{&InMemoryPlayerStore{}}
 	log.Fatal(http.ListenAndServe(":5000", server))
