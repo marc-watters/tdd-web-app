8ccda6b4cd3459e172502773151e18662c698148
Marc Watters <marc.watters@proton.me>
2025-02-07 16:50:01 -0500

    fix(test): satisfy test for storing player wins
    Run the tests and it should be passing! Obviously "Marc" isn't exactly
    what we want to send to RecordWin, so let's further refine the test.

--- server.go
+++ server.go
@@ -36,5 +36,6 @@ func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 }
 
 func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	p.store.RecordWin("Marc")
 	w.WriteHeader(http.StatusAccepted)
 }
