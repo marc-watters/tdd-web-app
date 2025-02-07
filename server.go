6ab92467b9974827ae7aad91c4a99e1d6d8782ea
Marc Watters <marc.watters@proton.me>
2025-02-07 16:33:36 -0500

    rfxr(feat): add helper PlayerServer.showScore()

--- server.go
+++ server.go
@@ -19,9 +19,10 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusAccepted)
 		return
 	}
+}
 
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-
 	score := p.store.GetPlayerScore(player)
 
 	if score == 0 {
