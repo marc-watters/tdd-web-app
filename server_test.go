e5ce8d603171fbf422fc3c370269358de958e8ee
Marc Watters <marc.watters@proton.me>
2025-02-09 01:20:50 -0500

    rfxr(test): implement helper assertContentType()

--- server_test.go
+++ server_test.go
@@ -134,6 +134,7 @@ func TestLeague(t *testing.T) {
 
 		got := getLeagueFromResponse(t, response.Body)
 		assertStatusCode(t, response.Code, http.StatusOK)
+		assertContentType(t, response, jsonContentType)
 		assertLeague(t, got, wantedLeague)
 
 		if response.Result().Header.Get("content-type") != "application/json" {
@@ -184,5 +185,12 @@ func assertLeague(t testing.TB, got, want []Player) {
 	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("\ngot: \t%v\nwant:\t%v", got, want)
+	}
+}
+
+func assertContentType(t testing.TB, response *httptest.ResponseRecorder, want string) {
+	t.Helper()
+	if response.Result().Header.Get("content-type") != want {
+		t.Errorf("response did not have content-type of %s, got %v", want, response.Result().Header)
 	}
 }
