c2dbb0542b5933bec56c6f4700272d9990c2a7c6
Marc Watters <marc.watters@proton.me>
2025-02-09 07:04:52 -0500

    add(test): use file system player store to get player score

--- file_system_store_test.go
+++ file_system_store_test.go
@@ -27,4 +27,20 @@ func TestFileSystemStore(t *testing.T) {
 		got = store.GetLeague()
 		assertLeague(t, got, want)
 	})
+
+	t.Run("get player score", func(t *testing.T) {
+		database := strings.NewReader(`[
+				{"Name": "Cleo", "Wins": 10},
+				{"Name": "Marc", "Wins": 20}
+			]`)
+
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetPlayerScore("Marc")
+		want := 20
+
+		if got != want {
+			t.Errorf("\ngot: \t%d\nwant:\t%d", got, want)
+		}
+	})
 }
