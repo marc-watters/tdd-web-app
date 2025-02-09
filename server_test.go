898b73af1411dba57cb319f25fc7e5e335c5e0d3
Marc Watters <marc.watters@proton.me>
2025-02-08 20:57:07 -0500

    add(test): validate server response as JSON
    You could argue a simpler initial step would be just to assert that the
    response body has a particular JSON string.
    
    In my experience tests that assert against JSON strings have the
    following problems.
    
    - Brittleness. If you change the data-model your tests will fail.
    - Hard to debug. It can be tricky to understand what the actual problem
    is when comparing two JSON strings.
    - Poor intention. Whilst the output should be JSON, what's really
    important is exactly what the data is, rather than how it's encoded.
    - Re-testing the standard library. There is no need to test how the
    standard library outputs JSON, it is already tested. Don't test other
    people's code.
    
    Instead, we should look to parse the JSON into data structures that are
    relevant for us to test with.

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -96,6 +97,13 @@ func TestLeague(t *testing.T) {
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
+
+		var got []Player
+
+		err := json.NewDecoder(response.Body).Decode(&got)
+		if err != nil {
+			t.Fatalf("unable to parse response from server %q into slice of Player, '%v'", response.Body, err)
+		}
 
 		assertStatusCode(t, response.Code, http.StatusOK)
 	})
