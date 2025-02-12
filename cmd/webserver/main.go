8408b8a0169c8b5741b783579c75e45ceab99b0c
Marc Watters <marc.watters@proton.me>
2025-02-12 14:58:33 -0500

    add(feat): create cmd/webserver package

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	poker "webapp/v2"
 )
 
 const dbFilename = "game.db.json"
@@ -14,11 +16,11 @@ func main() {
 		log.Fatalf("unable to open db file %s: %v", dbFilename, err)
 	}
 
-	store, err := NewFileSystemPlayerStore(db)
+	store, err := poker.NewFileSystemPlayerStore(db)
 	if err != nil {
 		log.Fatalf("problem creating file system player store: %v", err)
 	}
-	server := NewPlayerServer(store)
+	server := poker.NewPlayerServer(store)
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000: %v", err)
