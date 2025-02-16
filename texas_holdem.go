8dcd86ee3c2caa143cbc8fd223b1667d7fd2c2e2
Marc Watters <marc.watters@proton.me>
2025-02-16 06:58:10 -0500

    add(feat): update TexasHoldem.Start() to utilize generic io.Writer

--- texas_holdem.go
+++ texas_holdem.go
@@ -2,7 +2,6 @@ package poker
 
 import (
 	"io"
-	"os"
 	"time"
 )
 
@@ -24,7 +23,7 @@ func (g *TexasHoldem) Start(numberOfPlayers int, alertsDestination io.Writer) {
 	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
 	blindTime := 0 * time.Second
 	for _, blind := range blinds {
-		g.alerter.ScheduleAlertAt(blindTime, blind, os.Stdout)
+		g.alerter.ScheduleAlertAt(blindTime, blind, alertsDestination)
 		blindTime = blindTime + blindIncrement
 	}
 }
